3b6a6961879a7dca91817fcb798971fc42c66ea1
qllove520 <145273208+qllove520@users.noreply.github.com>
2025-08-22 10:02:30 +0800

    Delete Genreporttool2.5-main directory

--- Genreporttool2.5-main/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from ui.main_window import MainApplication
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainApplication()
-    window.show()
-    sys.exit(app.exec_())
